c8057487e8a9d3c9da19e6f2812d72ff9abb0ae9
HE Junyang <nathanhejunyang@gmail.com>
2019-11-06 18:05:35 +0100

    Update backend, a method in program controller to proxy cogedim program detail page.

--- server/src/main/java/com/catprogrammer/cogedimscanner/controller/ProgramController.kt
+++ server/src/main/java/com/catprogrammer/cogedimscanner/controller/ProgramController.kt
@@ -4,10 +4,13 @@ import com.catprogrammer.cogedimscanner.entity.Program
 import com.catprogrammer.cogedimscanner.model.ProgramDateLotDto
 import com.catprogrammer.cogedimscanner.service.ProgramService
 import com.fasterxml.jackson.annotation.JsonView
+import org.apache.commons.io.IOUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URL
+import java.nio.charset.StandardCharsets
 import java.time.LocalDate
 
 @RestController
@@ -18,7 +21,19 @@ open class ProgramController {
     @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
     @GetMapping("/programs")
     @JsonView(Program.SimpleView::class)
-    open fun findAllGroupByProgramNumber(): List<ProgramDateLotDto>  {
+    open fun findAllGroupByProgramNumber(): List<ProgramDateLotDto> {
         return programService.findProgramsGroupByProgramNumber()
+    }
+
+    @PreAuthorize("hasAuthority('WRITE_PRIVILEGE')")
+    @GetMapping("/program")
+    open fun fetchProgramPageHtml(url: String): String {
+        if (url.startsWith("https://www.cogedim.com/")) {
+            return IOUtils.toString(
+                    URL(url).openConnection().getInputStream(),
+                    StandardCharsets.UTF_8
+            )
+        }
+        return "URL must starts with https://www.cogedim.com/"
     }
 }
--- server/src/main/java/com/catprogrammer/cogedimscanner/utils/JwtTokenUtil.java
+++ server/src/main/java/com/catprogrammer/cogedimscanner/utils/JwtTokenUtil.java
@@ -32,17 +32,16 @@ public class JwtTokenUtil implements Serializable {
     private static final String SECRET = Credentials.jwtSecret;
 
 
-
     /**
      * 签发JWT
      */
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>(16);
-        claims.put( CLAIM_KEY_USERNAME, userDetails.getUsername() );
+        claims.put(CLAIM_KEY_USERNAME, userDetails.getUsername());
         return Jwts.builder()
-                .setClaims( claims )
-                .setExpiration( new Date( Instant.now().toEpochMilli() + EXPIRATION_TIME  ) )
-                .signWith( SignatureAlgorithm.HS512, SECRET )
+                .setClaims(claims)
+                .setExpiration(new Date(Instant.now().toEpochMilli() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SECRET)
                 .compact();
     }
 
@@ -51,46 +50,42 @@ public class JwtTokenUtil implements Serializable {
      */
     public Boolean validateToken(String token, UserDetails userDetails) {
         User user = (User) userDetails;
-        String username = getUsernameFromToken( token );
+        String username = getUsernameFromToken(token);
 
-        return (username.equals( user.getUsername() ) && !isTokenExpired( token ));
+        return (username.equals(user.getUsername()) && !isTokenExpired(token));
     }
 
     /**
      * 获取token是否过期
      */
-    public Boolean isTokenExpired(String token) {
-        Date expiration = getExpirationDateFromToken( token );
-        return expiration.before( new Date() );
+    private Boolean isTokenExpired(String token) {
+        Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
     }
 
     /**
      * 根据token获取username
      */
     public String getUsernameFromToken(String token) {
-        String username = getClaimsFromToken( token ).getSubject();
-        return username;
+        return getClaimsFromToken(token).getSubject();
     }
 
     /**
      * 获取token的过期时间
      */
-    public Date getExpirationDateFromToken(String token) {
-        Date expiration = getClaimsFromToken( token ).getExpiration();
-        return expiration;
+    private Date getExpirationDateFromToken(String token) {
+        return getClaimsFromToken(token).getExpiration();
     }
 
     /**
      * 解析JWT
      */
     private Claims getClaimsFromToken(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey( SECRET )
-                .parseClaimsJws( token )
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
                 .getBody();
-        return claims;
     }
-
 
 
 }
