477f5093457f0bbf59dd9b8e96053f679086e7ba
NathanKun <nathanhejunyang@gmail.com>
2019-11-10 22:42:54 +0100

    front end update program service, to ger program page main info div and delivery info div

--- client/src/app/service/program.service.ts
+++ client/src/app/service/program.service.ts
@@ -12,7 +12,7 @@ import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 export class ProgramService extends BaseService {
 
   private programDateLotCache: ProgramDateLot[];
-  private programPageCache: Map<string, SafeHtml>; // program url  => html
+  private programPageCache: Map<string, string>; // program url  => html str
 
   constructor(private http: HttpClient,
               private sanitizer: DomSanitizer) {
@@ -58,41 +58,102 @@ export class ProgramService extends BaseService {
       );
   }
 
+  public getProgramPageDeliveryInfo(url: string): Observable<SafeHtml> {
+    return this.getProgramPageMainInfo(url).pipe(
+      map(
+        html => {
+          const doc = new DOMParser().parseFromString(html.toString(), 'text/html');
+          const div = doc.querySelector('.informations');
+          return this.sanitizer.bypassSecurityTrustHtml(div.innerHTML);
+        }
+      )
+    );
+  }
+
+  public getProgramPageMainInfo(url: string): Observable<SafeHtml> {
+    return this.fetchProgramPage(url).pipe(
+      map(
+        str => {
+          const doc = new DOMParser().parseFromString(str, 'text/html');
+          const div = doc.querySelector('#main_info');
+
+          // convert <v-icon> to material icons
+          div.querySelectorAll('v-icon').forEach(
+            vIcon => {
+              const iMatIcon = document.createElement('i');
+              iMatIcon.innerHTML = vIcon.innerHTML;
+              iMatIcon.setAttribute('class', 'material-icons icons');
+              vIcon.parentNode.replaceChild(iMatIcon, vIcon);
+            }
+          );
+
+          // convert <address> to span
+          div.querySelectorAll('address').forEach(
+            addr => {
+              const span = document.createElement('span');
+              span.innerHTML = addr.innerText;
+              span.setAttribute('class', 'line');
+              addr.parentNode.replaceChild(span, addr);
+            }
+          );
+
+          // remove .event-button
+          div.querySelectorAll('.event-button').forEach(b => b.remove());
+
+          // remove v-btn
+          div.querySelectorAll('v-btn').forEach(b => b.remove());
+
+          // remove .hidden-xs, info may be duplicated with .hidden-xl
+          div.querySelectorAll('.hidden-xs').forEach(b => b.remove());
+
+          // remove .anchors
+          div.querySelectorAll('.anchors').forEach(b => b.remove());
+
+          return this.sanitizer.bypassSecurityTrustHtml(div.innerHTML);
+        }
+      )
+    );
+  }
+
   public getProgramPageSalesInfo(url: string): Observable<SafeHtml> {
+    return this.fetchProgramPage(url).pipe(
+      map(
+        str => {
+          const doc = new DOMParser().parseFromString(str, 'text/html');
+          const div = doc.querySelector('.paragraphs-sales_office');
+
+          // update images src, add cogedim's domain
+          div.querySelectorAll('img[src^="/"]').forEach(
+            e => e.setAttribute('src', 'https://www.cogedim.com/' + e.getAttribute('src'))
+          );
+
+          // remove sales office section
+          div.querySelector('.sales-office').remove();
+
+          // sanitize html
+          return this.sanitizer.bypassSecurityTrustHtml(div.innerHTML);
+        }
+      )
+    )
+  }
+
+  private fetchProgramPage(url: string): Observable<string> {
     if (this.programPageCache.has(url)) {
       return of(this.programPageCache.get(url));
     } else {
-      return this.fetchProgramPageSalesInfo(url);
-    }
-  }
-
-  private fetchProgramPageSalesInfo(url: string): Observable<SafeHtml> {
-    return this.http.get<string>(
-      this.baseurl + '/program?url=' + url,
-      {responseType: 'text' as 'json'})
-      .pipe(
-        map(
-          str => {
-            const doc = new DOMParser().parseFromString(str, 'text/html');
-            const div = doc.querySelector('.paragraphs-sales_office');
-
-            // update images src, add cogedim's domain
-            div.querySelectorAll('img[src^="/"]').forEach(
-              e => e.setAttribute('src', 'https://www.cogedim.com/' + e.getAttribute('src'))
-            );
-
-            // remove sales office section
-            div.querySelector('.sales-office').remove();
-
-            // sanitize html
-            const res = this.sanitizer.bypassSecurityTrustHtml(div.innerHTML);
-
+      return this.http.get<string>(
+        this.baseurl + '/program?url=' + url,
+        {responseType: 'text' as 'json'}
+      ).pipe(
+        tap(
+          res => {
             // cache result
             this.programPageCache.set(url, res);
-
             return res;
           }
         )
       );
+    }
+
   }
 }
